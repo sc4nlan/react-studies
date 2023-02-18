534dc98b900f0c3b98c97fa8e2df7fd963b46257
sc4nlan <santiago.rodrigues81@gmail.com>
2023-02-17 22:20:17 -0300

    #7. Utilizando React fragments

---
+++ meu-projeto/src/7-UtilizandoReactFragments/components/Item.js
@@ -0,0 +1,11 @@
+function Item(props){
+    return(
+        <>
+            <p>Produto:</p>
+            <li>{props.marca}</li>
+            
+        </>
+    );
+}
+
+export default Item;
---
+++ meu-projeto/src/7-UtilizandoReactFragments/components/List.js
@@ -0,0 +1,14 @@
+import Item from './Item'
+
+function List(){
+    return(
+        <>
+            <ul>
+                <Item marca="ferrari"/>
+                <Item marca="LG"/>
+            </ul>
+        </>
+    )
+}
+
+export default List;
