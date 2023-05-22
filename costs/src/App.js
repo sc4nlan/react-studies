34ebb63da6f7b34c17b91ca7051bb4f584b3f305
santiago <santiago@clio.nied.unicamp.br>
2023-05-22 02:30:32 -0300

    configurado pastas

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,25 +1,26 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+
 import Home from './componets/pages/Home'
 import Contact from './componets/pages/Contact';
 import NewProject from './componets/pages/NewProject';
 
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Container from './componets/layout/Container';
+import Navbar from './componets/layout/Navbar';
+import Footer from './componets/layout/Footer'
 
 function App() {
   return (
     <BrowserRouter>
-        <div>
-            <Link to="/">Home</Link>|
-            <Link to="/newproject">Contact</Link>|
-            <Link to="/contact">NewProject</Link>
-        </div>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/newproject" element={<NewProject/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-        </Routes>
-        <p>Footer</p>
+        <Navbar/>
+        <Container>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/newproject" element={<NewProject/>}/>
+              <Route path="/contact" element={<Contact/>}/>
+          </Routes>
+        </Container>
+        <Footer/>
     </BrowserRouter>
-
   )
 }
 
